1c3dc0836533debbd836460b53dc64655114f44f
marianne linardos <linardosmarianne@gmail.com>
2019-12-04 21:37:59 -0500

    ex 1

---
+++ w3resourceExercises/1/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine(-1+4*6);
+			Console.WriteLine((35+5)%7);
+			Console.WriteLine(14 + -4 * 6 / 11);
+			Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+			Console.ReadLine();
+
+		}	}
+}
