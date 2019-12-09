cd34e07a0ac106ff6c5111a3ba317345e1e43bb9
Marianne <marianneklinardos@gmail.com>
2019-12-08 19:20:29 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/CsPG-10/CsPG-10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CsPG_10 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Even or Odd");
+			Console.WriteLine("Please enter an integer: ");
+			string numberString = Console.ReadLine();
+			int number = Convert.ToInt16(numberString);
+			
+			if(number%2==0) {
+				Console.WriteLine($"The number {number} is EVEN.");
+			}
+			else {
+				Console.WriteLine($"The number {number} is ODD.");
+			}
+			Console.ReadKey();
+		}
+	}
+}
