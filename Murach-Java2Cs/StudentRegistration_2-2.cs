dfb42b5a85516fc9e46769d260cce90949d89073
Marianne <marianneklinardos@gmail.com>
2019-12-11 22:40:45 -0500

    Add files via upload

---
+++ Murach-Java2Cs/StudentRegistration_2-2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Student Registration Form");
+
+			Console.WriteLine("Enter First Name: ");
+			String firstName = Console.ReadLine();
+
+			Console.WriteLine("Enter Last Name: ");
+			String lastName = Console.ReadLine();
+
+			Console.WriteLine("Enter Year of Birth: ");
+			String stringYOB = Console.ReadLine();
+			//int yOB = Convert.ToInt32(StringYOB);
+
+			Console.WriteLine($"Welcome {firstName} {lastName}!");
+			Console.WriteLine("Your Registration is complete.");
+			Console.WriteLine($"Your temporary password is: {firstName}*{stringYOB}");
+
+			Console.ReadKey();
+		}
+	}
+}
