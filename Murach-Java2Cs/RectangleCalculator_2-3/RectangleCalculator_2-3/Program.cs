ae213863d79dda56076d5aa19bf4c354e3b0e108
Marianne <marianneklinardos@gmail.com>
2019-12-12 22:07:40 -0500

    Add files via upload
    Still trying to get continue loop to work

--- Murach-Java2Cs/RectangleCalculator_2-3/RectangleCalculator_2-3/Program.cs
+++ Murach-Java2Cs/RectangleCalculator_2-3/RectangleCalculator_2-3/Program.cs
@@ -1,38 +1,36 @@
 ﻿using System;
 
 namespace RectangleCalculator_2_3 {
-	class Program {
+	static class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Rectangle Calculator");
-			string choice = "Y";
 
-			while(choice == "Y") {
 
 			// Get Input
-				Console.WriteLine("Enter length: ");
-				
-				string strLength = Console.ReadLine();
-				decimal length = Convert.ToDecimal(strLength);
+			Console.WriteLine("Enter length: ");
 
-				Console.WriteLine("Enter width:  ");
-				string strWidth = Console.ReadLine();
-				decimal width = Convert.ToDecimal(strWidth);
+			string strLength = Console.ReadLine();
+			decimal length = Convert.ToDecimal(strLength);
+
+			Console.WriteLine("Enter width:  ");
+			string strWidth = Console.ReadLine();
+			decimal width = Convert.ToDecimal(strWidth);
 
 			// Calculate
-				decimal area = length * width;
-				decimal perimeter = (2 * width) + (2 * length);
+			decimal area = length * width;
+			decimal perimeter = (2 * width) + (2 * length);
 
 			// Output
-				Console.WriteLine($"Area:        {area}");
-				Console.WriteLine($"Perimeter:   {perimeter}");
+			Console.WriteLine($"Area:        {area}");
+			Console.WriteLine($"Perimeter:   {perimeter}");
 
-				Console.WriteLine("Continue?(Y/N): ");
-				string strChoice = Console.ReadLine();
-				choice = choice.ToUpper();
-				Console.ReadKey();
 
-			 }
+			
+
 			Console.WriteLine("Have a Fantastic Day!!");
+			Console.ReadKey();
+
 		}
+
 	}
-}
+}	 
