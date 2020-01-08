8f1a731f58da5ba4d5c0011bab801753ed04b384
Marianne <mlinardos.edu@gmail.com>
2020-01-07 19:56:14 -0500

    Update Program.cs

--- w3resourceExercises/1/1/Program.cs
+++ w3resourceExercises/1/1/Program.cs
@@ -6,6 +6,7 @@ namespace _4 {
 			Console.WriteLine(-1+4*6);
 			
 			Console.WriteLine((35+5)%7);
+			
 			Console.WriteLine(14 + -4 * 6 / 11);
 			Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
 			Console.ReadLine();
