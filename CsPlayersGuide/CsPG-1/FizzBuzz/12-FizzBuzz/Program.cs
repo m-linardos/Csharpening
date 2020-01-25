b29225fdb7ecabf59b1da3aecb90ed0eebee0b79
Marianne <marianneklinardos@gmail.com>
2020-01-25 14:40:18 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/FizzBuzz/12-FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPG_12_FizzBuzz {
+	class Program {
+		static void Main(string[] args) {
+			int num = 0;
+			// Loop through #s 1-100
+			for(num = 1; num < 101; num++) 
+			{					
+				if(num % 3 == 0) {
+					Console.WriteLine("Fizz");						
+				}
+				 else if(num % 5 == 0) {
+					Console.WriteLine("Buzz");
+				 }
+				 else if(num % 3 == 0 && num % 5 == 0) {
+					Console.WriteLine("FizzBuzz");
+				 }
+				 else {
+					Console.WriteLine(num);
+				 }
+			}
+			Console.ReadKey();
+
+		}
+	}
+}
