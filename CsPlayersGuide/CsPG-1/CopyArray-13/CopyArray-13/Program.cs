ff32b03c7b8579272c113997b07674e3be06311e
Marianne <marianneklinardos@gmail.com>
2020-01-25 15:40:45 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/CopyArray-13/CopyArray-13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyArray_13 {
+	class Program {
+		static void Main(string[] args) {
+		// Create array
+			string[] family = new[] { "Mom", "Dad", "Me", "Caitlin", "Joey", "Petey", "Phill", "Sammy", "John", "Brian", "Danny", "Susan", "Kristen", "Craig" };
+			// Copy value from family array to a copy
+			string[] familyCopy = new string[family.Length];
+			for(int i = 0; i < family.Length; i++) {
+				familyCopy[i] = family[i];
+			}	 
+
+			// Print original array
+			for(int i = 0; i < family.Length; i++) {
+				Console.Write(family[i] + " ");
+			}
+			Console.WriteLine();
+
+			// Print copy
+			for(int i = 0; i < familyCopy.Length; i++)	{
+				Console.WriteLine(familyCopy[i] + " ");
+			}
+			Console.WriteLine();
+
+			// Print the number of family members
+			int numberOfMembers = family.Length;
+			Console.WriteLine($"There are {numberOfMembers}	family members.");
+			Console.WriteLine();
+			
+
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ CsPlayersGuide/CsPG-1/CopyArray-13/CopyArray-13/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CopyArray-13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CopyArray-13")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6cd4e9ae-2e16-488c-9157-cb0ea5b4299a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
