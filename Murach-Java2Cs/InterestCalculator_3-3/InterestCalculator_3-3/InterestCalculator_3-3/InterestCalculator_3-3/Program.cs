a0492c9e8232f449176566797da3e853bd662366
Marianne <marianneklinardos@gmail.com>
2020-01-24 23:52:38 -0500

    Add files via upload

---
+++ Murach-Java2Cs/InterestCalculator_3-3/InterestCalculator_3-3/InterestCalculator_3-3/InterestCalculator_3-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator_3_3 {
+	class Program {
+		static void Main(string[] args) {
+
+			Console.WriteLine("Welcome to Interest Calculator");
+
+			Console.WriteLine("Enter loan amount: ");
+			string strLoanAmt = Console.ReadLine();
+			decimal loanAmt = Convert.ToDecimal(strLoanAmt);
+			strLoanAmt = String.Format("Loan amount: {0, 0:C2}", loanAmt);
+			
+
+			Console.WriteLine("Enter interest rate %: ");
+			string strIntRate = Console.ReadLine();
+			decimal intRate = Convert.ToDecimal(strIntRate);
+			intRate = intRate/100;
+			strIntRate = String.Format("Interest rate: {0, 0:F2}", intRate);
+			
+
+			decimal intCalc = loanAmt * intRate;
+			string strIntCalc = String.Format("Interest: {0, 0:C2}", intCalc);
+
+			Console.WriteLine(strLoanAmt);
+			Console.WriteLine(strIntRate);
+			Console.WriteLine(strIntCalc);
+
+			Console.ReadKey();
+
+
+
+
+
+
+		}
+	}
+}
