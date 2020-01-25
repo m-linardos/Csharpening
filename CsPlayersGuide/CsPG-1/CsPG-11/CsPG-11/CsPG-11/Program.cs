30adb39683420c14613e7d68eca5cc5262910de7
Marianne <marianneklinardos@gmail.com>
2020-01-25 02:46:34 -0500

    Add files via upload

--- CsPlayersGuide/CsPG-1/CsPG-11/CsPG-11/CsPG-11/Program.cs
+++ CsPlayersGuide/CsPG-1/CsPG-11/CsPG-11/CsPG-11/Program.cs
@@ -8,13 +8,13 @@ namespace CsPG_11 {
 	class Program {
 		static void Main(string[] args) {
 
-			Console.WriteLine("Welcome to Simple Calculator");
+			Console.WriteLine("	Welcome to Simple Calculator");
 			Console.WriteLine();
 			Console.WriteLine();
 			bool isOperand = true;
 
 			// Get operand
-			Console.Write("What math function would you like. Choose from  + , - , * , / , % : " );
+			Console.Write("	What math function would you like. Choose from  + , - , * , / , % : " );
 			Console.WriteLine();
 			string operand = Console.ReadLine();
 			switch(operand) {
@@ -23,14 +23,14 @@ namespace CsPG_11 {
 				case "*":
 				case "/":
 				case "%":
-					Console.WriteLine("Good choice!");
+					Console.WriteLine("	Good choice!");
 					Console.WriteLine();
 					break;
 				default:
 					Console.WriteLine();
-					Console.WriteLine("That is not an option");
+					Console.WriteLine("	That is not an option");
 					Console.WriteLine();
-					Console.Write("What math function would you like.  Choose from  + , - , * , / , % : ");
+					Console.Write("	What math function would you like.  Choose from  + , - , * , / , % : ");
 					Console.WriteLine();
 					operand = Console.ReadLine();
 					switch(operand) {
@@ -40,12 +40,12 @@ namespace CsPG_11 {
 						case "/":
 						case "%":
 							Console.WriteLine();
-							Console.WriteLine("Good choice!");
+							Console.WriteLine("	Good choice!");
 							Console.WriteLine();
 							break;
 						default:
 							Console.WriteLine();
-							Console.WriteLine("REALLY !!!  I'm done with with you !!!");
+							Console.WriteLine("	REALLY !!!  I'm done with with you !!!");
 							isOperand = false;
 							break;
 					}
@@ -56,12 +56,12 @@ namespace CsPG_11 {
 			if(isOperand) {
 				// Get inputs & convert to double
 				Console.WriteLine();
-				Console.Write("Enter a number: ");
+				Console.Write("	Enter a number: ");
 				string num1Str = Console.ReadLine();
 				double num1 = Convert.ToDouble(num1Str);
 
 				Console.WriteLine();
-				Console.Write("Enter another number: ");
+				Console.Write("	Enter another number: ");
 				string num2Str = Console.ReadLine();
 				double num2 = Convert.ToDouble(num2Str);
 
@@ -86,7 +86,7 @@ namespace CsPG_11 {
 				}
 				// Print out the answer
 				Console.WriteLine();
-				Console.WriteLine($"{num1} {operand} {num2} = {result}");
+				Console.WriteLine($"	{num1} {operand} {num2} = {result}");
 			} else { }		
 			 
 			Console.ReadKey();		 	
