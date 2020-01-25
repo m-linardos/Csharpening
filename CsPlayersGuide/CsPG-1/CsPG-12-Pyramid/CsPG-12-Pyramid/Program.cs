32983dc3e68f7317903ee257e0634dab36d903c9
Marianne <marianneklinardos@gmail.com>
2020-01-25 14:04:12 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/CsPG-12-Pyramid/CsPG-12-Pyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPG_12_Pyramid {
+	class Program {
+		static void Main(string[] args) {
+
+			Console.WriteLine("Pyramid");
+
+			// Add a line
+			for(int row = 0; row < 5; row++) {
+				// Print spaces
+				for(int space = 0; space < 5 - row - 1; space++) {
+					Console.Write(" ");
+				}
+				// Print stars
+				for(int star = 0; star < row * 2 + 1; star++) {
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+			}
+			Console.ReadKey();
+		}
+	}
+}
