6f6ce8d6630a6f34d830276aff046fbe2a4b0f1b
Marianne <marianneklinardos@gmail.com>
2020-02-05 20:36:04 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/BuildClasses-18/BuildClasses-18/Color.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildClasses_18 {
+	class Color {
+		private int red;
+		private int green;
+		private int blue;
+		private int alpha;
+
+		public Program(int red, int green, int blue, int alpha) {
+			this.red = red;
+			this.green = green;
+			this.blue = blue;
+			this.alpha = alpha;
+
+		}
+
+		public Program(int red, int green, int blue) {
+			this.red = red;
+			this.green = green;
+			this.blue = blue;
+			this.alpha = 255;
+
+		}
+
+		//public  MyProperty {
+		//	get { return green; }
+		//	set { green = value; }
+		//}
+
+
+		//public int MyProperty {
+		//	get { return red; }
+		//	set { red = value; }
+		//}
+	}
+}
