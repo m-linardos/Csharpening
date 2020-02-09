f5194c21ba63d254c6f5f7ece06986b0936f0d98
Marianne <marianneklinardos@gmail.com>
2020-02-08 23:44:19 -0500

    Add files via upload

---
+++ CsPlayersGuide/CsPG-1/BuildClasses-18/BuildClasses-18/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildClasses_18 {
+	/// <summary>
+	/// This represents a ball described by color, size & number of times thrown. 
+	/// </summary>
+	class Ball {
+	//
+		private Color color;
+		private int circumfrance;
+		private int passes;
+
+
+		/// <summary>
+		/// Popped the ball.
+		/// </summary>
+		public void Popped() {
+			this.circumfrance = 0;
+		}
+
+		/// <summary>
+		/// Throws the ball, adding it to the number of passes.
+		/// </summary>
+		/// <param name="passes">The passes.</param>
+		public void Throw (int passes) {
+			if(circumfrance != 0) {
+				this.passes = passes + 1;
+			}
+		}
+
+		/// <summary>
+		/// Gets the passes.
+		/// </summary>
+		/// <returns>passes</returns>
+		public int GetPasses() {
+			return passes;
+		}
+
+	}
+}
